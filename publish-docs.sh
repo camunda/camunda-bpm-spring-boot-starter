107dc142ace32dde7b5eb411e18d021b5b7b5907
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-09-01 14:51:46 +0200

    chore(docs): add docs infrastructure script
    - add script to publish build doc on gh-pages branch
    
    closes #15

---
+++ publish-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GIT_COMMIT=${1:-`git rev-parse --short HEAD`}
+GH_PAGES_DIR=github-pages
+rm -rf $GH_PAGES_DIR
+git clone git@github.com:camunda/camunda-bpm-spring-boot-starter.git $GH_PAGES_DIR
+(cd $GH_PAGES_DIR && git checkout gh-pages)
+cp -r camunda-spring-boot-starter-docs/target/generated-docs/* $GH_PAGES_DIR
+(cd $GH_PAGES_DIR && git add . && git commit -m "Push docs for commit $GIT_COMMIT" && git push)
