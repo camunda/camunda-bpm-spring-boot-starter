bcbb258ae49f806fd35f3f3470945f73dec6b99d
Oliver Steinhauer <oliver.steinhauer@gmail.com>
2016-07-09 11:06:02 +0200

    changed configuration to work with devtools #101

--- extension/camunda-spring-boot-starter/src/main/java/org/camunda/bpm/spring/boot/starter/CamundaBpmAutoConfiguration.java
+++ extension/camunda-spring-boot-starter/src/main/java/org/camunda/bpm/spring/boot/starter/CamundaBpmAutoConfiguration.java
@@ -1,11 +1,21 @@
 package org.camunda.bpm.spring.boot.starter;
 
-import org.camunda.bpm.container.RuntimeContainerDelegate;
-import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ExternalTaskService;
+import org.camunda.bpm.engine.FilterService;
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngineServices;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.interceptor.CommandExecutor;
 import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,95 +23,77 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 
-import javax.annotation.PostConstruct;
-
-
-@EnableConfigurationProperties({CamundaBpmProperties.class, CamundaBpmManagementProperties.class})
-@Import({CamundaBpmConfiguration.class, CamundaBpmActuatorConfiguration.class, CamundaBpmPluginsConfiguration.class})
+@EnableConfigurationProperties({ CamundaBpmProperties.class, CamundaBpmManagementProperties.class })
+@Import({ CamundaBpmConfiguration.class, CamundaBpmActuatorConfiguration.class, CamundaBpmPluginsConfiguration.class })
 @AutoConfigureAfter(HibernateJpaAutoConfiguration.class)
-public class CamundaBpmAutoConfiguration implements ProcessEngineServices {
+public class CamundaBpmAutoConfiguration {
 
   @Bean
-  public static ProcessEngineFactoryBean processEngineFactoryBean(final ProcessEngineConfigurationImpl processEngineConfigurationImpl) {
+  public ProcessEngineFactoryBean processEngineFactoryBean(final ProcessEngineConfigurationImpl processEngineConfigurationImpl) {
     final ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
     factoryBean.setProcessEngineConfiguration(processEngineConfigurationImpl);
 
     return factoryBean;
   }
 
-  @Autowired
-  private ProcessEngine processEngine;
-
   @Bean(name = "runtimeService")
-  @Override
-  public RuntimeService getRuntimeService() {
-    return processEngine.getRuntimeService();
+  public RuntimeService getRuntimeService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getRuntimeService();
   }
 
   @Bean(name = "repositoryService")
-  @Override
-  public RepositoryService getRepositoryService() {
-    return processEngine.getRepositoryService();
+  public RepositoryService getRepositoryService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getRepositoryService();
   }
 
   @Bean(name = "formService")
-  @Override
-  public FormService getFormService() {
-    return processEngine.getFormService();
+  public FormService getFormService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getFormService();
   }
 
   @Bean(name = "taskService")
-  @Override
-  public TaskService getTaskService() {
-    return processEngine.getTaskService();
+  public TaskService getTaskService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getTaskService();
   }
 
   @Bean(name = "historyService")
-  @Override
-  public HistoryService getHistoryService() {
-    return processEngine.getHistoryService();
+  public HistoryService getHistoryService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getHistoryService();
   }
 
   @Bean(name = "identityService")
-  @Override
-  public IdentityService getIdentityService() {
-    return processEngine.getIdentityService();
+  public IdentityService getIdentityService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getIdentityService();
   }
 
   @Bean(name = "managementService")
-  @Override
-  public ManagementService getManagementService() {
-    return processEngine.getManagementService();
+  public ManagementService getManagementService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getManagementService();
   }
 
   @Bean(name = "authorizationService")
-  @Override
-  public AuthorizationService getAuthorizationService() {
-    return processEngine.getAuthorizationService();
+  public AuthorizationService getAuthorizationService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getAuthorizationService();
   }
 
   @Bean(name = "caseService")
-  @Override
-  public CaseService getCaseService() {
-    return processEngine.getCaseService();
+  public CaseService getCaseService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getCaseService();
   }
 
   @Bean(name = "filterService")
-  @Override
-  public FilterService getFilterService() {
-    return processEngine.getFilterService();
+  public FilterService getFilterService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getFilterService();
   }
 
   @Bean(name = "externalTaskService")
-  @Override
-  public ExternalTaskService getExternalTaskService() {
-    return processEngine.getExternalTaskService();
+  public ExternalTaskService getExternalTaskService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getExternalTaskService();
   }
 
   @Bean(name = "decisionService")
-  @Override
-  public DecisionService getDecisionService() {
-    return processEngine.getDecisionService();
+  public DecisionService getDecisionService(ProcessEngineServices processEngineServices) {
+    return processEngineServices.getDecisionService();
   }
 
   @Bean
