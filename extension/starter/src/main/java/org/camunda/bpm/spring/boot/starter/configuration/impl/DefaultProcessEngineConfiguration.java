ee25095071ca3fb561ea69136ac2a77eb51fe1ea
Jan Galinski <jan.galinski@holisticon.de>
2017-04-02 18:41:15 +0200

    make idGenerator configurable, fixes #224

--- extension/starter/src/main/java/org/camunda/bpm/spring/boot/starter/configuration/impl/DefaultProcessEngineConfiguration.java
+++ extension/starter/src/main/java/org/camunda/bpm/spring/boot/starter/configuration/impl/DefaultProcessEngineConfiguration.java
@@ -1,20 +1,30 @@
 package org.camunda.bpm.spring.boot.starter.configuration.impl;
 
+import org.camunda.bpm.engine.impl.cfg.IdGenerator;
 import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.camunda.bpm.spring.boot.starter.configuration.CamundaProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+
+import java.util.Optional;
 
 public class DefaultProcessEngineConfiguration extends AbstractCamundaConfiguration implements CamundaProcessEngineConfiguration {
 
+  @Autowired
+  private Optional<IdGenerator> idGenerator;
+
   @Override
   public void preInit(SpringProcessEngineConfiguration configuration) {
-    String processEngineName = StringUtils.trimAllWhitespace(camundaBpmProperties.getProcessEngineName());
-    if (!StringUtils.isEmpty(processEngineName) && !processEngineName.contains("-")) {
-      configuration.setProcessEngineName(processEngineName);
-    } else {
-      logger.warn("Ignoring invalid processEngineName='{}' - must not be null, blank or contain hyphen", camundaBpmProperties.getProcessEngineName());
-    }
+    setProcessEngineName(configuration);
+    setDefaultSerializationFormat(configuration);
+    setIdGenerator(configuration);
+  }
 
+  private void setIdGenerator(SpringProcessEngineConfiguration configuration) {
+    idGenerator.ifPresent(configuration::setIdGenerator);
+  }
+
+  private void setDefaultSerializationFormat(SpringProcessEngineConfiguration configuration) {
     String defaultSerializationFormat = camundaBpmProperties.getDefaultSerializationFormat();
     if (StringUtils.hasText(defaultSerializationFormat)) {
       configuration.setDefaultSerializationFormat(defaultSerializationFormat);
@@ -22,5 +32,15 @@ public class DefaultProcessEngineConfiguration extends AbstractCamundaConfigurat
       logger.warn("Ignoring invalid defaultSerializationFormat='{}'", defaultSerializationFormat);
     }
   }
+
+  private void setProcessEngineName(SpringProcessEngineConfiguration configuration) {
+    String processEngineName = StringUtils.trimAllWhitespace(camundaBpmProperties.getProcessEngineName());
+    if (!StringUtils.isEmpty(processEngineName) && !processEngineName.contains("-")) {
+      configuration.setProcessEngineName(processEngineName);
+    } else {
+      logger.warn("Ignoring invalid processEngineName='{}' - must not be null, blank or contain hyphen", camundaBpmProperties.getProcessEngineName());
+    }
+  }
+
 
 }
