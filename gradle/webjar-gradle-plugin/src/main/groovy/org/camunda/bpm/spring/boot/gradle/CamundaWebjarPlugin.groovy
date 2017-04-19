97c48364281301e9dfb4bc90b6e811a942ad13e1
Stefan Zilske <stefan.zilske@holisticon.de>
2017-04-19 11:27:27 +0200

    Support for Enterprise Webapps with Gradle (fixes #216) (#222) (fixes #231)
    * initial version
    
    * adding gradle wrapper
    
    * adding example project for Gradle
    
    * rename package
    
    * rename main class
    
    * Updating instructions in README
    
    * Adapting documentation
    
    * Ability to pass the camundaVersion from the build.gradle to the plugin
    
    * Passing camunda version from example build.gradle
    
    * Updating README

---
+++ gradle/webjar-gradle-plugin/src/main/groovy/org/camunda/bpm/spring/boot/gradle/CamundaWebjarPlugin.groovy
@@ -0,0 +1,42 @@
+package org.camunda.bpm.spring.boot.gradle;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class CamundaWebjarPlugin implements Plugin<Project> {
+
+  void apply(Project project) {
+    def myExt = project.extensions.create("camundaWebjarPlugin", CamundaWebjarPluginExtension)
+
+    project.configurations {
+      camundaEE {
+        extendsFrom compile
+      }
+    }
+
+    project.task("resolveCamundaEnterpriseWebjar") {
+
+      doLast {
+        project.configurations.camundaEE.dependencies.add(project.dependencies.create("org.camunda.bpm.webapp:camunda-webapp-ee-plugins:$myExt.camundaVersion@war"))
+
+        project.copy {
+          from project.configurations.camundaEE.collect { project.zipTree(it) }
+          into "$project.buildDir/resources/main"
+        }
+
+        project.delete "$project.buildDir/resources/main/META-INF"
+
+        project.copy {
+          from "$project.buildDir/resources/main/WEB-INF/securityFilterRules.json"
+          into "$project.buildDir/resources/main"
+        }
+
+        project.delete "$project.buildDir/resources/main/WEB-INF"
+      }
+    }
+  }
+}
+
+class CamundaWebjarPluginExtension {
+  String camundaVersion
+}
