f69ef5575eef16d179145020b2d0227413d3cd41
adakl <adakl@192.168.2.139>
2018-03-18 14:03:59 +0100

    feat(webjar): Works now with the new structure of webjar form pull request #289

--- starter-webapp-core/src/main/java/org/camunda/bpm/spring/boot/starter/webapp/CamundaBpmWebappAutoConfiguration.java
+++ starter-webapp-core/src/main/java/org/camunda/bpm/spring/boot/starter/webapp/CamundaBpmWebappAutoConfiguration.java
@@ -22,46 +22,46 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @AutoConfigureAfter(CamundaBpmAutoConfiguration.class)
 public class CamundaBpmWebappAutoConfiguration extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
+  @Autowired
+  private ResourceLoader resourceLoader;
 
-    @Autowired
-    private CamundaBpmProperties properties;
+  @Autowired
+  private CamundaBpmProperties properties;
 
 
-    @Bean
-    public CamundaBpmWebappInitializer camundaBpmWebappInitializer(CamundaBpmProperties properties) {
-        return new CamundaBpmWebappInitializer(properties);
+  @Bean
+  public CamundaBpmWebappInitializer camundaBpmWebappInitializer(CamundaBpmProperties properties) {
+    return new CamundaBpmWebappInitializer(properties);
+  }
+
+  @Bean(name = "resourceLoaderDependingInitHook")
+  public InitHook<ResourceLoaderDependingFilter> resourceLoaderDependingInitHook() {
+    return filter -> {
+      filter.setResourceLoader(resourceLoader);
+      filter.setWebappProperty(properties.getWebapp());
+    };
+  }
+
+  @Bean
+  public LazyInitRegistration lazyInitRegistration() {
+    return new LazyInitRegistration();
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    final String classpath = "classpath:" + properties.getWebapp().getWebjarClasspath();
+    registry.addResourceHandler("/lib/**").addResourceLocations(classpath + "/lib/");
+    registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
+    registry.addResourceHandler("/app/**").addResourceLocations(classpath + "/app/");
+    super.addResourceHandlers(registry);
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    if (properties.getWebapp().isIndexRedirectEnabled()) {
+      registry.addRedirectViewController("/", "/app/");
     }
-
-    @Bean(name = "resourceLoaderDependingInitHook")
-    public InitHook<ResourceLoaderDependingFilter> resourceLoaderDependingInitHook() {
-        return filter -> {
-            filter.setResourceLoader(resourceLoader);
-            filter.setWebappProperty(properties.getWebapp());
-        };
-    }
-
-    @Bean
-    public LazyInitRegistration lazyInitRegistration() {
-        return new LazyInitRegistration();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        final String classpath = "classpath:" + properties.getWebapp().getWebjarClasspath();
-        registry.addResourceHandler("/lib/**").addResourceLocations(classpath + "/lib/");
-        registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
-        registry.addResourceHandler("/app/**").addResourceLocations(classpath + "/app/");
-        super.addResourceHandlers(registry);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        if (properties.getWebapp().isIndexRedirectEnabled()) {
-            registry.addRedirectViewController("/", "/app/");
-        }
-        super.addViewControllers(registry);
-    }
+    super.addViewControllers(registry);
+  }
 
 }
