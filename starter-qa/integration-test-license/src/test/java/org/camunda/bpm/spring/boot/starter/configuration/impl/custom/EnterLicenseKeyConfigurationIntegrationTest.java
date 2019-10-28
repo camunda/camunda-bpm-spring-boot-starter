49bc1dfb5030ad65092810712f6c972d3118e7ed
Miklas Boskamp <miklas.boskamp@camunda.com>
2019-10-28 09:22:07 +0100

    feat(engine): Support new license format
    Related to CAM-10667

--- starter-qa/integration-test-license/src/test/java/org/camunda/bpm/spring/boot/starter/configuration/impl/custom/EnterLicenseKeyConfigurationIntegrationTest.java
+++ starter-qa/integration-test-license/src/test/java/org/camunda/bpm/spring/boot/starter/configuration/impl/custom/EnterLicenseKeyConfigurationIntegrationTest.java
@@ -1,8 +1,9 @@
 /*
- * Copyright © 2015 - 2018 camunda services GmbH and various authors (info@camunda.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -15,25 +16,44 @@
  */
 package org.camunda.bpm.spring.boot.starter.configuration.impl.custom;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+
+import org.assertj.core.api.Assertions;
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cmd.LicenseCmd;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.spring.boot.starter.configuration.impl.custom.EnterLicenseKeyConfiguration;
 import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.camunda.bpm.spring.boot.starter.test.helper.StandaloneInMemoryTestConfiguration;
 import org.camunda.bpm.spring.boot.starter.util.CamundaBpmVersion;
-import org.camunda.bpm.spring.boot.starter.util.CamundaBpmVersionTest;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.net.URL;
+public class EnterLicenseKeyConfigurationIntegrationTest {
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class EnterLicenseKeyConfigurationTest {
-
-  protected static final URL LICENSE_FILE_URL = EnterLicenseKeyConfiguration.class.getClassLoader().getResource("camunda-license-dummy.txt");
+  private static final URL LICENSE_FILE_URL = EnterLicenseKeyConfigurationIntegrationTest.class.getClassLoader().getResource("camunda-license-dummy.txt");
 
   @Rule
   public ProcessEngineRule processEngineRule = new StandaloneInMemoryTestConfiguration().rule();
+  
+  ProcessEngine processEngine;
+  ManagementService managementService;
+
+  
+  @Before
+  public void init() {
+    processEngine = processEngineRule.getProcessEngine();
+    managementService = processEngine.getManagementService();
+  }
 
   @Test
   public void shouldLoadStringFromUrl() throws Exception {
@@ -60,6 +80,9 @@ public class EnterLicenseKeyConfigurationTest {
     // then
     String licenseKey = enterLicenseKeyConfiguration.getLicenseKey(processEngineRule.getProcessEngine());
     assertValidLicenseKeyFormat(licenseKey);
+    // make sure new license API was used
+    String licenseProperty = managementService.getProperties().get(LicenseCmd.LICENSE_KEY_BYTE_ARRAY_ID);
+    assertThat(licenseProperty).isNotNull();
   }
 
   protected void assertValidLicenseKeyFormat(String licenseKey) {
@@ -70,16 +93,30 @@ public class EnterLicenseKeyConfigurationTest {
       .doesNotContain("\n");
   }
 
-  protected EnterLicenseKeyConfiguration createEnterLicenseKeyConfiguration() {
+  private EnterLicenseKeyConfiguration createEnterLicenseKeyConfiguration() {
 
     CamundaBpmProperties properties = new CamundaBpmProperties();
     properties.setLicenseFile(LICENSE_FILE_URL);
 
-    CamundaBpmVersion version = CamundaBpmVersionTest.camundaBpmVersion("123-ee");
+    CamundaBpmVersion version = camundaBpmVersion("123-ee");
     EnterLicenseKeyConfiguration enterLicenseKeyConfiguration = new EnterLicenseKeyConfiguration();
     ReflectionTestUtils.setField(enterLicenseKeyConfiguration, "version", version);
     ReflectionTestUtils.setField(enterLicenseKeyConfiguration, "camundaBpmProperties", properties);
 
     return enterLicenseKeyConfiguration;
+  }
+
+  private CamundaBpmVersion camundaBpmVersion(final String version) {
+    final Package pkg = mock(Package.class);
+    when(pkg.getImplementationVersion()).thenReturn(version);
+    Constructor<CamundaBpmVersion> camBpmVersionConstructor;
+    try {
+      camBpmVersionConstructor = CamundaBpmVersion.class.getDeclaredConstructor(Package.class);
+      camBpmVersionConstructor.setAccessible(true);
+      return camBpmVersionConstructor.newInstance(pkg);
+    } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+      Assertions.fail("Error creating Camunda Version", e);
+      return null;
+    }
   }
 }
