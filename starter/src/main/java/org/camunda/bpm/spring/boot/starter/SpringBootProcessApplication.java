484bdde36ed4550ba5d74b483c6302e93734a4bd
Tobias Metzke <tobias.metzke@camunda.com>
2019-11-15 08:19:57 +0100

    fix(starter): enable custom context path
    * add `@Configuration` annotation to `SpringBootProcessApplication`
      because nested `@Configuration` classes are only scanned for
      classes that are annotated with `@Component` since Spring 5.1.x
    
    related to CAM-10187

--- starter/src/main/java/org/camunda/bpm/spring/boot/starter/SpringBootProcessApplication.java
+++ starter/src/main/java/org/camunda/bpm/spring/boot/starter/SpringBootProcessApplication.java
@@ -41,10 +41,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.ServletContextAware;
 
+@Configuration
 public class SpringBootProcessApplication extends SpringProcessApplication {
 
   @Bean
@@ -117,12 +119,8 @@ public class SpringBootProcessApplication extends SpringProcessApplication {
   }
 
   @ConditionalOnWebApplication
-  @Bean
-  public WebApplicationConfiguration myWebAppConfiguration() {
-    return new WebApplicationConfiguration();
-  }
-
-  public class WebApplicationConfiguration implements ServletContextAware {
+  @Configuration
+  class WebApplicationConfiguration implements ServletContextAware {
 
     @Override
     public void setServletContext(ServletContext servletContext) {
